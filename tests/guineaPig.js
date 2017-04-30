4559a9bbc984a807457ac4915bdd6e3d37d1b093
davidmashe <davidmashe@gmail.com>
2017-04-30 12:08:08 -0400

    removed issues

--- tests/guineaPig.js
+++ tests/guineaPig.js
@@ -10,7 +10,7 @@ module.exports = {
       .click("body > div.position-relative.js-header-wrapper > header > div > div > div > a:nth-child(2)")
       .waitForElementVisible('#login_field')
       .waitForElementVisible('#password')
-      .setValue('#login_field', 'davidmashe')
+      .setValue('#login_field', process.env.GITHUB_LOGIN)
       .setValue('#password', process.env.GITHUB_PASSWORD)
       .click('#login > form > div.auth-form-body.mt-3 > input.btn.btn-primary.btn-block')
       .pause(1000)
